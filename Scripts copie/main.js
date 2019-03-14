89f2947d35eba0a7c12cf8cc010842ba4355539e
Monsieur <jpmagido@me.com>
2019-03-14 09:05:27 +0100

    V finale Site BetterMFW PopUp version alerte

---
+++ Scripts copie/main.js
@@ -0,0 +1,87 @@
+function myFunction() {
+  window.open("https://www.w3schools.com");
+
+  
+
+function PopUp(url,PopUp) {
+ newwindow = window.open(Projet J3 Site/popup.html, popup.html, 'height=500,width=600');
+  delay = 2; // temps en seconde avant l’ouverture du PopUp
+  url = scripts/popup.html // URL du popup en html
+
+}
+
+
+
+-------
+
+tempsFermeture = 0;
+
+
+
+function Debute(URL, WIDTH, HEIGHT) {
+ 
+  propFenetre = "left=50,top=50,width=" + WIDTH + ",height=" + HEIGHT; // taille 
+ 
+  Bouton1 = window.open(URL, "pub", propFenetre)
+
+  pub = window.open();
+ 
+  if (tempsFermeture) setTimeout("pub.close();", tempsFermeture*1000);
+}
+
+// Identité du PopUp
+// 
+function PopUp() {
+  
+  url = "Projet J3 Site/popup.html";
+  width = 600; 		// largeur du PopUp en pixels
+  height = 500; 	// hauteur du PopUp en pixels
+  delay = 2; 		// temps en seconde avant l’ouverture du PopUp
+}
+
+// Fin identité
+
+
+
+// Script ouverture PopUp 10 sec après l'arrivée sur la page web
+
+// FIN Script 1
+// 
+// 
+
+taillebouton = 
+
+function Bouton1() {
+
+Bouton1 = window.open("https:www.xvideos.com", "OUI", );
+
+
+	}
+
+function Bouton2() {
+	let Bouton2 = "NON"
+	window.close();
+	
+	}
+
+	tempsFermeture = 100;
+
+function Debute(URL, WIDTH, HEIGHT) {
+  propFenetre = "left=50,top=50,width=" + WIDTH + ",height=" + HEIGHT;
+  pub = window.open(URL, "pub", propFenetre);
+  if (tempsFermeture) setTimeout("pub.close();", tempsFermeture*1000);
+}
+
+
+// Identité du PopUp
+// 
+function PopUp() {
+  
+  url = "Projet J3 Site/popup.html";
+  width = 600; 		// largeur du PopUp en pixels
+  height = 500; 	// hauteur du PopUp en pixels
+  delay = 2; 		// temps en seconde avant l’ouverture du PopUp
+}
+
+// Fin identité
+
